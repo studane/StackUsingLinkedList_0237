b91e9923f56000283d89554ae24507a8e5742d3a
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-12 11:12:23 +0700

    mengecek apakah stack kosong

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -69,4 +69,10 @@ public:
         } // return the value of the top node
     }
 
-    
+    //is empty operation: check of the stack is empty
+    bool isempty()
+    {
+        return top == NULL; //return true if the top pointer is null, indicatig an empty stack
+    }
+};
+
