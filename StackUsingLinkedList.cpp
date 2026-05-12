73e99c895e7cb4bec2a84539aaaa914e709f5690
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-12 11:14:20 +0700

    menambah opsi program

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -77,3 +77,19 @@ public:
 };
 
 int main()
+{
+    stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice !=5)
+    {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.exit\n";
+        cout << "enter your choice:";
+        cin >> choice;
+
+        
