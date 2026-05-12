10004d5c50441a5ae3638c88e13ef7f93b0d1002
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-12 11:10:43 +0700

    menambah if else di fungsi peek

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -51,4 +51,22 @@ public:
 
     //peel/top operation: Retrive the value of the topset element without removing 
     void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empthy." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "  " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        
+        } // return the value of the top node
+    }
+
     
